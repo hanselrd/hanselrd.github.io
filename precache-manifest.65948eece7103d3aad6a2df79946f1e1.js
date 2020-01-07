f3d10e63a97d41fbbc9a9471b189d28b63c33f46
Hansel De La Cruz <hansel809@hotmail.com>
2020-01-07 10:29:38 -0500

    Updates

---
+++ precache-manifest.65948eece7103d3aad6a2df79946f1e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a4c9b4b14c98439dde28ed9aa4adfb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a92f43691ca5c74df79d",
+    "url": "./static/css/main.fdd01500.chunk.css"
+  },
+  {
+    "revision": "e5c1dec9de8f1ca23f12",
+    "url": "./static/js/2.6668d9b2.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "./static/js/2.6668d9b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a92f43691ca5c74df79d",
+    "url": "./static/js/main.9b756118.chunk.js"
+  },
+  {
+    "revision": "f786f926713acc0226d2",
+    "url": "./static/js/runtime-main.8d70acf7.js"
+  }
+]);
